9fb5cc544e56e23fab110b53555f61622feab75e
muhammadfaatihaflah-del <muhammadfaatihaflah@gmail.com>
2026-05-04 11:41:19 +0700

    men definisi kan  semua constructor

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -13,3 +13,16 @@ class mahasiswa{
     mahasiswa(int iNim, string iNama);
     void cetak();
 };
+
+mahasiswa :: mahasiswa(){
+}
+mahasiswa :: mahasiswa(int iNim){
+    nim = iNim; // definisi hanya NIM
+}
+mahasiswa :: mahasiswa(string iNama){
+    nama = iNama; // definisi hanya NAMA
+}
+mahasiswa :: mahasiswa(int iNim, string iNama){
+    nim = iNim;   // definisi NIM dan NAMA
+    nama = iNama;
+}
